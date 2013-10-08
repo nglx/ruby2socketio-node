09277dee7ff8bd6c206e98a055875eebe68fabf1
Marcin Naglik <marcin.naglik@gmail.com>
2013-10-09 00:55:21 +0200

    adding config and start io server

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
-var fs = require("fs");
+var fs = require("fs"),
+	config = require("./config").config;
 
 var app = require("http").createServer(handler), // handler defined below
 io = require("socket.io").listen(app);
@@ -20,3 +21,23 @@ function handler (req, res) {
 theport = process.env.PORT || 2000;
 app.listen(theport);
 console.log ("http server on port: " + theport);
+
+startIOServer();
+
+function startIOServer () {
+    console.log("Starting ...");
+
+    // Many hosted environments do not support all transport forms currently, (specifically WebSockets).
+    // So we force a relatively safe xhr-polling transport.
+    // Modify io.configure call to allow other transports.
+
+    io.configure(function () { 
+    	io.set("transports", config[platform].transports); // Set config in ./config.js
+    	io.set("polling duration", 10); 
+	io.set("log level", 2);
+    });
+    io.sockets.on("connection", function (socket) {
+	readAndSend(socket);
+    });
+};
+
